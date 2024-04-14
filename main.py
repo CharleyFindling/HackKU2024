36776e54a5aeb668e3f0eb74e40a519cda6bd0eb
Harrison Reed <91584420+hreed88@users.noreply.github.com>
2024-04-14 00:28:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ from bson import json_util, ObjectId
 from datetime import datetime, timedelta
 from flask import Blueprint, request, jsonify
 
-
+#Name: get_deb
+#Description: creates a "link" to the data-base, as db
+#####################Create Database##############################
 def get_db():
     """
     Configuration method to return db instance
     """
     db = pymongo.MongoClient("mongodb+srv://harrisonreed16:7ZMa1gtOuhoBBwyw@foodapp.ruxmkcf.mongodb.net/?retryWrites=true&w=majority&appName=FoodApp")
-    db = db['FooApp']
+    db = db['FooApp']['db']
     if db is None:
 
         db = g._database = PyMongo(current_app).db
@@ -34,6 +36,8 @@ def get_db():
        
     return db
 
+#Name: MongoJsonEncoder
+#Description: Creates the Json encoder for the mongo data-base
 class MongoJsonEncoder(DefaultJSONProvider):
     def default(self, obj):
         if isinstance(obj, datetime):
@@ -41,13 +45,16 @@ class MongoJsonEncoder(DefaultJSONProvider):
         if isinstance(obj, ObjectId):
             return str(obj)
         return DefaultJSONProvider.default(obj)
-
-
+    
+#Name: create_app
+#Description: Creates the application by assigning various values to a newly created flask app
+#Return: returns the new app
+########################Create App##############################
 def create_app():
     
     APP_DIR = os.path.abspath(os.path.dirname(__file__))
     STATIC_FOLDER = os.path.join(APP_DIR, '/static/')
-    TEMPLATE_FOLDER = APP_DIR + '/templates'#os.path.join(APP_DIR, '/templates')
+    TEMPLATE_FOLDER = APP_DIR + '/templates'
     print(TEMPLATE_FOLDER)
 
     app = Flask(__name__, static_folder=STATIC_FOLDER,
@@ -61,23 +68,120 @@ def create_app():
     @app.route('/<path:path>')
     def serve(path):
         return render_template('/index.html')
-
-    #db.add_restuarant("NONE" , "NONE", "NONE", "NONE")
-
     return app
+###############################################################
+#Create config parser
 config = configparser.ConfigParser()
 config.read('config/.ini')
+
+#Create the application, and assign configuration
 app = create_app()
 app.config['DEBUG'] = True
 app.config['MONGO_URI'] = config['PROD']['DB_URI']
+
 # Use LocalProxy to read the global db instance with just `db`
-######################### Handleing ############################
 db = LocalProxy(get_db)
-def resuarant_push(userName, password, name, address):
-    temp = {str(userName):[{'password': password, 'Restuarant: ' : name, 'Address: ' : address}]}
+
+#Description: Does work when on corresponding HTML methods
+######################### Handling ############################
+def resuarant_push(userName, password, isBusiness,isIndividual,isOrg, name,driversLicense, dob, address):
+    temp =  {"db":{
+            "Entity":[
+            {
+                "userName": str(userName),
+                "password": str(password),
+                "isBusiness": bool(isBusiness),
+                "isIndividual": bool(isIndividual),
+                "isOrg": bool(isOrg),
+                "Name": str("Hello"),
+                "location": str(address),
+                "driversLicense": str(driversLicense),
+                "dob": str(dob)
+            }
+        ],
+
+        "food": [
+            {
+                "foodId": 0,
+                "foodName": "",
+                "foodBestBy": "",
+                "foodQuantity": 0,
+                "foodPostedBy": "",
+                "foodLocation": "",
+                "pickUpTime": ""
+            }
+        ]
+    }
+    }
+    #{str(userName):[{'password': password, 'Restuarant: ' : name, 'Address: ' : address}]}
     print(db.Restuarants.insert_one(temp))
 
+def food_push(foodId, foodName, foodBestBy, foodQuantity, foodPostedBy, foodLocation, pickUpTime):
+    print(db)
+    
+    print(db.update_one(
+        {
+            "Entity.Name" : "Hello"
+        }
+        ,
+        {
+            "$set" : {
+                "food.foodId": foodId,
+                "food.foodName": foodName,
+                "food.foodBestBy": foodBestBy,
+                "food.foodQuantity":10000,
+                "food.foodPostedBy": foodPostedBy,
+                "food.foodLocation": foodLocation,
+                "food.pickUpTime": pickUpTime
+            }
+        }
 
+    ))
+    template = {
+                "foodId": foodId,
+                "foodName": foodName,
+                "foodBestBy": foodBestBy,
+                "foodQuantity":10000,
+                "foodPostedBy": foodPostedBy,
+                "foodLocation": foodLocation,
+                "pickUpTime": pickUpTime
+                }
+    
+    print("\n\n",db.update_many({"db.Entity.Name" : "Hello"}, {"$set": {"db.food": template}}, upsert=True))
+    # db.entity.update_one({"name": "Hello"},
+    #             {"$set":{
+    #             "foodId": foodId,
+    #             "foodName": foodName,
+    #             "foodBestBy": foodBestBy,
+    #             "foodQuantity":10000,
+    #             "foodPostedBy": foodPostedBy,
+    #             "foodLocation": foodLocation,
+    #             "pickUpTime": pickUpTime
+    #             }
+    #             }
+    # )
+
+def food_update(foodId,foodQuantity):
+    print(db)
+
+    if(foodQuantity > 0):
+        foodQuantity -= 1
+        db.update_one({"foodId": 0},
+
+               {"$set":{
+                "foodQuantity": foodQuantity
+                }
+                      }
+    
+        )
+        if(foodQuantity==0):
+            deleteFoodByID(foodId)
+    
+    
+
+#Name: getAllFoodNearby
+#Description: Retrieves all elemnts that are near the current distance
+#Param: thisDistance - the distance to compare against
 def getAllFoodNearby(thisDistance):
     result = []
     curser = db.find({"distance": {"$gt": thisDistance, "$lt": thisDistance}})
@@ -86,10 +190,17 @@ def getAllFoodNearby(thisDistance):
     print(result)
     return result
 
+#Name: getByBusiness
+#Description: Retrieves the element in the data base that contains the business name
+#Param: businessName - the name to be retrieved
 def getByBusiness(bussinessName):
     print("By business")
     return db.find({"business" : bussinessName})
 
+
+#Name: getByFoodID
+#Description: Retrieves the element in the data base that contains the food element
+#Param: thisID - the id to be retrieved
 def getByFoodId(thisId):
     result = []
     curser = db.find({"foodId" : thisId})
@@ -97,21 +208,59 @@ def getByFoodId(thisId):
         result.append(curser)
     return result
 
+
+#Name: deleteFoodByID
+#Description: Deletes item contained in the data base based on the foods id number
+#Useful for when food becomes "out of stock"
+#Param: thisID - the id to be deleted
 def deleteFoodByID(thisID):
     db.remove({"foodId" : thisID})
     return
+#Description: Control flow section to handle HTML methods
 ###################### control flow ############################
 @app.route('/restuarant_push', methods =['POST'])
-def flow_restuarant_push(path):
+def flow_restuarant_push():
     
     #ADD WHEN READY 
     userName = request.args.get('uName')
     password = request.args.get('pWord')
     print(userName)
-    rest = request.args.get('rName')
+    isBusiness = request.args.get('isBusiness')
+    isIndividual = request.args.get('isIndividual')
+    isOrg = request.args.get('isOrg')
+    name = request.args.get('rName')
     address = request.args.get('name')
+    driversLicense = request.args.get('driversLicense')
+    dob = request.args.get('dob')
     try:
-        resuarant_push(userName, password, rest, address)
+        resuarant_push(userName, password, isBusiness,isIndividual,isOrg, name,driversLicense, dob, address)
+        return ("restuarant push") #render_template('submit.html')
+    except Exception as e:
+        print("Error", e)
+        return jsonify({'error': str("DARN")}), 400
+    
+@app.route('/food_push', methods=['POST'])
+def flow_food_push():
+    foodId = request.args.get('foodId')
+    foodName = request.args.get('foodName')
+    foodBestBy = request.args.get('foodBestBy')
+    foodQuantity = request.args.get('foodQuantity')
+    foodPostedBy = request.args.get('foodPostedBy')
+    foodLocation = request.args.get('foodLocation')
+    pickUpTime = request.args.get('pickUpTime')
+    try:
+        food_push(foodId, foodName, foodBestBy, foodQuantity, foodPostedBy, foodLocation, pickUpTime)
+        return ("food push") #render_template('submit.html')
+    except Exception as e:
+        print("Error", e)
+        return jsonify({'error': str("DARN")}), 400
+
+@app.route('/food_update', methods=['POST'])
+def flow_food_update():
+    foodId = request.args.get('foodId')
+    foodQuantity = request.args.get('foodQuantity')
+    try:
+        food_update(foodId,12)#foodQuantity)
         return ("restuarant push") #render_template('submit.html')
     except Exception as e:
         print("Error", e)
@@ -131,16 +280,21 @@ def flow_nearby_get():
         print("Error", e)
         return jsonify({'error': str("DARN")}), 400
     
-
+#Name:flow_byBusiness_get
+#Description: Calls the getByBusinessfunction when,
+#Directory of app is appended by /byBusiness and A GET call is made  
 @app.route('/byBusiness', methods=['GET'])
-def flow_byBussiness_get():
+def flow_byBusiness_get():
     business = request.args.get('business')
     try:
         return "busines by id"#getByBusiness(business)
     except Exception as e:
         print("Error", e)
         return jsonify({'error': str("DARN")}), 400
-    
+
+#Name:flow_getByFoodId
+#Description: Calls the getByFoodId function when,
+#Directory of app is appended by /byFoodId and A GET call is made     
 @app.route('/byFoodId', methods=['GET'])
 def flow_getByFoodId_get():
     fID = request.args.get('foodID')
@@ -150,6 +304,9 @@ def flow_getByFoodId_get():
         print("Error", e)
         return jsonify({'error': str("DARN")}), 400
 
+#Name:flow_deleteFood_delete
+#Description: Calls the delete by food function when,
+#Directory of app is appended by /delete and A DELETE call is made 
 @app.route('/delete', methods=['DELETE'])
 def flow_deleteFood_delete():
     fID = request.args.get('foodID')
@@ -158,14 +315,12 @@ def flow_deleteFood_delete():
     except Exception as e:
         print("Error", e)
         return jsonify({'error': str("DARN")}), 400
-# ###############################################################
+################################################################
 
 
 
-
+# Name: main
+# Description: Starts the application
 if __name__ == "__main__":
-    
-    
-    #get_db()
     app.run()
-    #uri = "mongodb+srv://harrisonreed16:7ZMa1gtOuhoBBwyw@foodapp.ruxmkcf.mongodb.net/?retryWrites=true&w=majority&appName=FoodApp"
+    
